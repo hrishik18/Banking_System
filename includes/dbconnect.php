59314900a8e0724c6bcc195e5d6c5a84eadba6b7
hrishik18 <hrishik.s@somaiya.edu>
2022-04-27 20:51:47 +0530

    image change

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -3,7 +3,7 @@ function OpenCon()
  {
  $dbhost = "localhost";
  $dbuser = "root";
- $dbpass = "";
+ $dbpass = "K)tB@D8vZ)JEQ(NK";
 //   put your password inside config . inc . php which is in  in XAMPP\PHPMYADMIN folder.
  $db = "bank";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
