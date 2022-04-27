91f4ddf9a33e081ae98fbf9f62548bb22865f36a
hrishik18 <hrishik.s@somaiya.edu>
2022-04-27 15:17:46 +0530

    feat(database): connection made to phpadmin with a  user

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,12 +1,17 @@
 <?php
-
-$server = 'localhost';
-$user = '';
-$password = '';
-$db = 'BMS';
-
-$con = mysqli_connect($server, $user, $password, $db);
-
-if (!$con) {
-    die('Could not connect');
-}
+function OpenCon()
+ {
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "";
+//   put your password inside config . inc . php which is in  in XAMPP\PHPMYADMIN folder.
+ $db = "bank";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+ return $conn;
+ }
+ 
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
