b6019ff98fac9538a68f382f31dfad80d156c0db
apeksha <apeksha05hegde@gmail.com>
2022-04-29 04:50:33 +0530

    side-nav

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,20 @@
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("cred_cont").classList.toggle("show");
+}
 
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+if (!event.target.matches('.dropbtn')) {
+
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
     }
-  });
+  }
+}
 }
